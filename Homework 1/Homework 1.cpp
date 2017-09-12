d6c3bf8d6ad11d31511aa07999487116ce05913e
vermas3 <vermas3@SOB-B2113-19>
2017-09-12 19:09:39 -0400

    Updated 1

--- Homework 1/Homework 1.cpp
+++ Homework 1/Homework 1.cpp
@@ -2,10 +2,47 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+
 
 
 int main()
 {
-    return 0;
+	float Cost = 0;
+	float AmountPaid = 0;
+	float Difference = Cost - AmountPaid;
+	float AmountMore = 0;
+
+	cout << " Cost " << endl;
+	cin >> Cost;
+
+	cout << " Amount Added " << endl;
+	cin >> AmountPaid;
+
+	while (true)
+	{
+
+		if (Cost > AmountPaid) {
+			Difference = Cost - AmountPaid;
+			cout << " The difference is " << Difference << endl;
+			cin >> AmountMore;
+			AmountPaid = AmountPaid + AmountMore;
+		}
+
+		else {
+
+			cout << "Thank you for your Purchase";
+			break;
+
+		}
+	}
+	return 0;
+
 }
+
+
+
+
+
 
