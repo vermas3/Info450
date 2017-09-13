c64f9232eb7131f0f989d80fa16b5f313770be28
vermas3 <vermas3@SOB-B2113-19>
2017-09-12 21:11:31 -0400

    Updated 2

--- Homework 1/Homework 1.cpp
+++ Homework 1/Homework 1.cpp
@@ -7,17 +7,29 @@ using namespace std;
 
 
 
+
+
+
 int main()
 {
 	float Cost = 0;
 	float AmountPaid = 0;
 	float Difference = Cost - AmountPaid;
 	float AmountMore = 0;
+	float yourchange = 0;
+	int dollars;
+	int quarters;
+	int dimes;
+	int nickels;
+	int ndollars;
+	int nquarters;
+	int ndimes;
+	int nnickels;
 
-	cout << " Cost " << endl;
+	cout << " Please enter the Cost of the item " << endl;
 	cin >> Cost;
 
-	cout << " Amount Added " << endl;
+	cout << " Please enter the amount paid " << endl;
 	cin >> AmountPaid;
 
 	while (true)
@@ -25,17 +37,41 @@ int main()
 
 		if (Cost > AmountPaid) {
 			Difference = Cost - AmountPaid;
-			cout << " The difference is " << Difference << endl;
+			cout << " That is not enough! you need " << Difference << " more " << endl;
 			cin >> AmountMore;
 			AmountPaid = AmountPaid + AmountMore;
 		}
 
+		else if (Cost < AmountPaid) {
+			yourchange = AmountPaid - Cost;
+			cout << " Your change is " << yourchange << endl;
+			
+
+			ndollars = yourchange / 100;
+			yourchange = yourchange - (ndollars*1.0);
+			cout << yourchange << " Dollar(s) " << endl;
+				
+				
+
+				nquarters = yourchange / 25;
+				yourchange = yourchange - (nquarters*1.0);
+
+				ndimes = yourchange / 10;
+				yourchange = yourchange - (ndimes*1.0);
+
+				nnickels = yourchange / 5;
+				yourchange = yourchange - (nnickels*1.0);
+				break;
+
+			
+		}
 		else {
 
-			cout << "Thank you for your Purchase";
+			cout << " Thank you for your Purchase. ";
 			break;
 
 		}
+			
 	}
 	return 0;
 
